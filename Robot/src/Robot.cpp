3a6eb5ce4a5054dea81c4d79c73960a0dcab1c6c
hoffmantj <hoffmant@uchicago.edu>
2019-01-02 23:34:19 -0600

    Allowed angles between 0 and 180 degrees.

--- Robot/src/Robot.cpp
+++ Robot/src/Robot.cpp
@@ -17,12 +17,12 @@ int distance = 100;
 // Max possible motor speed mapped to 8 bits (0-255 in integer).
 // 255 is full power (5V), 0 is no power (0V)
 const int max_speed = 255;
-// calibration constant between (1-0) for the ratio of torque 
+// calibration constant between (1-0) for the ratio of torque
 // between left and right wheel.  0.5 is an even split
 float calibration = 0.5;
 // High speed setting. Currently makes motor speed 2.5V (ie 0.5*255)
 float high_speed = 0.5;
-float low_speed = 0.0; 
+float low_speed = 0.0;
 
 int left_wheel_high = static_cast<unsigned int>(max_speed * high_speed * calibration);
 int right_wheel_high = static_cast<unsigned int>(max_speed * high_speed * (1-calibration));
@@ -45,12 +45,12 @@ int readPing(){
 
 void initialize() {
     Serial.begin(9600);
-  
+
     pinMode(RightMotorForward, OUTPUT);
     pinMode(LeftMotorForward, OUTPUT);
     pinMode(LeftMotorBackward, OUTPUT);
     pinMode(RightMotorBackward, OUTPUT);
-  
+
     servo_motor.attach(11); //our servo pin
 
     servo_motor.write(90);
@@ -84,7 +84,7 @@ void moveForward() {
     if (!movingForward) {
 
         movingForward=true;
-    
+
         analogWrite(LeftMotorForward, left_wheel_high);
         analogWrite(RightMotorForward, right_wheel_high);
 
@@ -95,14 +95,14 @@ void moveForward() {
 
 void moveBackward() {
     movingForward=false;
-  
+
     if (!movingBackward) {
 
         movingBackward = true;
 
         digitalWrite(LeftMotorBackward, left_wheel_high);
         digitalWrite(RightMotorBackward, right_wheel_high);
-  
+
         digitalWrite(LeftMotorForward, left_wheel_low);
         digitalWrite(RightMotorForward, left_wheel_low);
     }
@@ -121,7 +121,7 @@ void moveStop() {
 void turnRight(int duration) {
     digitalWrite(LeftMotorBackward, left_wheel_high);
     digitalWrite(RightMotorForward, right_wheel_high);
-  
+
     digitalWrite(LeftMotorForward, left_wheel_low);
     digitalWrite(RightMotorBackward, right_wheel_low);
 
@@ -139,12 +139,12 @@ void turnRight(int duration) {
 void turnLeft(int duration) {
     digitalWrite(LeftMotorForward, left_wheel_high);
     digitalWrite(RightMotorBackward, right_wheel_high);
-  
+
     digitalWrite(LeftMotorBackward, left_wheel_low);
     digitalWrite(RightMotorForward, right_wheel_low);
-  
+
     delay(duration);
-  
+
     if (movingForward) {
         moveForward();
     } else if (movingBackward) {
@@ -154,13 +154,13 @@ void turnLeft(int duration) {
     }
 }
 
-int lookForward() {
+int getDistance() {
     readPing();
 }
 
 int lookAngle(int angle) {
-    if (angle < 10) { angle = 10; }
-    if (angle > 170) { angle = 170; }
+    if (angle < 0) { angle = 0; }
+    if (angle > 180) { angle = 180; }
 
     servo_motor.write(angle);
     delay(500);
